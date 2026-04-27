7cceadec8f74d33862cb6541d635ee85e6834e5d
jilk27 <frolovkirill2007@yandex.ru>
2026-04-28 01:09:26 +0300

    Add files via upload

---
+++ boost_client2.cpp
@@ -0,0 +1,55 @@
+﻿// boost_client.cpp
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+
+using boost::asio::ip::tcp;
+
+void client(boost::asio::io_context& io_context, const std::string& host, const std::string& port) {
+    tcp::resolver resolver(io_context);
+    // Преобразуем имя хоста и порт в IP-адреса
+    auto endpoints = resolver.resolve(host, port);
+
+    tcp::socket socket(io_context);
+    // Подключаемся к серверу
+    boost::asio::connect(socket, endpoints);
+    std::cout << "Connected to server!" << std::endl;
+
+    boost::system::error_code ec;
+
+    while (true) {
+        // 2. Отправляем данные серверу
+        std::string message;
+        std::getline(std::cin, message);
+
+        if (message == "quit") {
+            std::cout << "Closing connection..." << std::endl;
+            boost::asio::write(socket, boost::asio::buffer(message), ec);
+            break;
+        }
+
+        message += '\n';
+
+        boost::asio::write(socket, boost::asio::buffer(message), ec);
+        if (ec) {
+            std::cerr << "Error sending data: " << ec.message() << std::endl;
+        }
+        else {
+            std::cout << "Client sent: " << message << std::endl;
+        }
+    }
+}
+
+int main() {
+    try {
+        boost::asio::io_context io_context;
+        client(io_context, "127.0.0.1", "12345");
+        std::cout << "\nPress Enter to exit...";
+        std::cin.get();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        std::cin.get();
+    }
+    return 0;
+}
---
+++ boost_server2.cpp
@@ -0,0 +1,79 @@
+﻿// boost_server.cpp
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using boost::asio::ip::tcp;
+
+std::string search_max(const std::string& input) {
+    std::istringstream iss(input);
+    int max = 0, num;
+    iss >> max;
+    while (iss >> num) {
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    std::string result = "Maximum: " + std::to_string(max);
+
+    return result;
+}
+
+// Функция для общения с одним подключенным клиентом
+void session(tcp::socket socket) {
+    try {
+
+        boost::asio::streambuf buffer;
+
+        boost::system::error_code ec;
+        while (true) {
+            size_t length = boost::asio::read_until(socket, buffer, "\n", ec);
+
+            // Извлекаем строку из буфера
+            std::istream input_stream(&buffer);
+            std::string client_message;
+            std::getline(input_stream, client_message);
+
+            if (client_message == "quit") {
+                std::cout << "Closing connection..." << std::endl;
+                break;
+            }
+
+            std::string res = search_max(client_message);
+
+            std::cout << res << std::endl;
+        }
+
+    }
+    catch (std::exception& e) {
+        std::cerr << "Session error: " << e.what() << std::endl;
+    }
+}
+
+void server(boost::asio::io_context& io_context, unsigned short port) {
+    // Создаем acceptor, который будет слушать порт
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
+    std::cout << "Server is listening on port " << port << "..." << std::endl;
+
+    while (true) {
+        tcp::socket socket(io_context);
+        // Ждем подключения клиента
+        acceptor.accept(socket);
+        std::cout << "Client connected!" << std::endl;
+        // Обрабатываем клиента
+        session(std::move(socket));
+    }
+}
+
+int main() {
+    try {
+        boost::asio::io_context io_context;
+        server(io_context, 12345);
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return 0;
+}
