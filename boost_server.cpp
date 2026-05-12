ad8158c9d5ee046b5d88876ab4d3f6a50b8219db
jilk27 <frolovkirill2007@yandex.ru>
2026-05-12 23:07:39 +0300

    Update boost_server.cpp

--- boost_server.cpp
+++ boost_server.cpp
@@ -6,18 +6,15 @@
 using boost::asio::ip::tcp;
 
 std::string caps_lock(const std::string& input) {
-    // Переводим строку в верхний регистр
     std::string upper_str = input;
     std::transform(upper_str.begin(), upper_str.end(), upper_str.begin(),
         [](unsigned char c) { return std::toupper(c); });
 
-    // Формируем результат: <длина>: <строка в верхнем регистре>
     std::string result = std::to_string(input.length()) + ": " + upper_str;
 
     return result;
 }
 
-// Функция для общения с одним подключенным клиентом
 void session(tcp::socket socket) {
     try {
         
@@ -27,7 +24,6 @@ void session(tcp::socket socket) {
         while(true){
             size_t length = boost::asio::read_until(socket, buffer, "\n", ec);
 
-            // Извлекаем строку из буфера
             std::istream input_stream(&buffer);
             std::string client_message;
             std::getline(input_stream, client_message);
@@ -49,16 +45,13 @@ void session(tcp::socket socket) {
 }
 
 void server(boost::asio::io_context& io_context, unsigned short port) {
-    // Создаем acceptor, который будет слушать порт
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), port));
     std::cout << "Server is listening on port " << port << "..." << std::endl;
 
     while(true){
         tcp::socket socket(io_context);
-        // Ждем подключения клиента
         acceptor.accept(socket);
         std::cout << "Client connected!" << std::endl;
-        // Обрабатываем клиента
         session(std::move(socket));
     }
 }
